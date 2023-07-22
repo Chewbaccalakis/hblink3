2657109f659878fc21ab35d873fd717e8229671b
Chewbaccalakis <nick@trochalakis.com>
2023-07-22 00:05:46 -0700

    Now i think

--- app/app.sh
+++ app/app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x /app/hblink3/start.sh
-/app/hblink3/start.sh
+/bin/bash /app/hblink3/start.sh
--- app/hblink3/start.sh
+++ app/hblink3/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash python3 ./hblink3/bridge.py
+python3 ./hblink3/bridge.py
